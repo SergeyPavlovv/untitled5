001e4f62832950c2fd02d7194ef52fda3a4db987
Sergey Pavlov <hainrihv@gmail.com>
2020-10-10 17:34:59 +0300

    Приветствие

---
+++ src/Name.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("fff");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя");
+        String name ;
+        name = scanner.next();
+        System.out.println("Ваше имя " + name);
+        scanner.close();
+
+    }
+}
