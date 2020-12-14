d49dec14ce0bea369f12cd06ccea48d9f99b7e09
Sergey Pavlov <hainrihv@gmail.com>
2020-12-14 23:29:51 +0300

    Algorithmization

--- src/com/unia/basicsUpSkill/algorithmization/arraysOfArrays/Task7.java
+++ src/com/unia/basicsUpSkill/algorithmization/arraysOfArrays/Task7.java
@@ -1,20 +1,41 @@
 package com.unia.basicsUpSkill.algorithmization.arraysOfArrays;
 
+import java.util.Scanner;
+
 public class Task7 {
 
     // Сформировать квадратную матрицу порядка N по правилу:
     //     A[i,j]=sin((i^2-j^2)/N)
     //и подсчитать количество положительных элементов в ней.
+
+
 // считать из консоли
 //создать и заполнить матрицу
     // вывести матрицу
     //подсчет положительных элементов и вывод
 
     public static void main(String[] args) {
-        int i = 2;
-        int j = 3;
-        int n = 2;
-        double d = Math.sin((Math.pow(i,2) - Math.pow(j,2))/ n);
-        System.out.println(d);
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double[][] matrix = new double[n][n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = (double) Math.sin((Math.pow(i, 2) - Math.pow(j, 2)) / n);
+                if (matrix[i][j] > 0) {
+                    count += 1;
+                }
+            }
+
+        }System.out.println("Положительных элементов в матрице: " + count);
+
+        for(int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
