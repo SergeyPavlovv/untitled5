805b281678e2448b333c6988f45ffc6398cbde6a
Sergey Pavlov <hainrihv@gmail.com>
2020-12-17 14:28:28 +0300

    Algorithmization

--- src/com/unia/basicsUpSkill/linearPrograms/Task6.java
+++ src/com/unia/basicsUpSkill/linearPrograms/Task6.java
@@ -6,39 +6,37 @@ import java.util.List;
 import java.util.Map;
 
 public class Task6 {
+
     //Для данной области составить линейную программу, которая печатает true,
     // если точка с координатами (х, у) принадлежит закрашенной области, и false — в противном случае:
 
     public static void main(String[] args) {
-        Map<Integer, List<Integer>> area = new HashMap<>();
-        for (int y = -3; y <= 7; y++) {
-            if (y == 0)
-                continue;
-            ArrayList<Integer> xList = new ArrayList<>();
-            if (y < 0) {
-                for (int x = -4; x <= 4; x++) {
-                    if (x == 0)
-                        continue;
-                    xList.add(x);
-                }
-            } else {
-                for (int x = -2; x <= 2; x++) {
-                    if (x == 0)
-                        continue;
-                    xList.add(x);
-                }
-            }
-            area.put(y,xList);
-        }
 
-        int x = 4;
-        int y = 5;
-        boolean res = false;
+        int x = 2;
+        int y = 1;
 
-        if(area.containsKey(y)){
-            res = area.get(y).contains(x);
-        }
+        int squareX = 2;
+        int squareX2 = -2;
+        int squareY = 4;
+        int squareY2 = 0;
 
-        System.out.println(res);
+        boolean squareCheckX = squareX >= x && squareX2 <= x;
+        boolean squareCheckY = squareY >= y && squareY2 <= y;
+        boolean square = squareCheckX && squareCheckY;
+
+
+        int rectangleX = 4;
+        int rectangleX2 = -4;
+        int rectangleY = -3;
+        int rectangleY2 = 0;
+
+        boolean rectangleCheckX = rectangleX >= x && rectangleX2 <= x;
+        boolean rectangleCheckY = rectangleY >= y && rectangleY2 <= y;
+        boolean rectangle = rectangleCheckX && rectangleCheckY;
+
+        boolean figure = square || rectangle;
+
+
+        System.out.println(figure);
     }
 }
