688bd061de34367b592c8c9a40defdc2e58c5461
Sergey Pavlov <hainrihv@gmail.com>
2020-12-20 23:22:47 +0300

    Algorithmization

---
+++ src/com/unia/basicsUpSkill/algorithmization/arraysOfArrays/Task15.java
@@ -0,0 +1,51 @@
+package com.unia.basicsUpSkill.algorithmization.arraysOfArrays;
+
+public class Task15 {
+
+    // Найдите наибольший элемент матрицы и заменить все нечетные элементы на него.
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25},
+        };
+        int max = 0;
+        max = maxElement(matrix);
+        System.out.println("Наибольший элемент матрицы: " + max);
+        changeElements(matrix, max);
+        printMatrix(matrix);
+    }
+
+    private static int maxElement(int[][] matrix) {
+        int max = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (max < matrix[i][j]) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    private static void changeElements(int[][] matrix, int max) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] % 2 != 0) {
+                    matrix[i][j] = max;
+                }
+            }
+        }
+    }
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
